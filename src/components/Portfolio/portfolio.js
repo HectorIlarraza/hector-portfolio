19c00baedd4985d54d482dcf3561e44e1947ea37
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2023-08-08 17:20:06 -0400

    stylized Portfolio component further

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -29,7 +29,7 @@ const Portfolio = () => {
                 <img className='portfolio-image' src={port.cover} alt="portfolio" />
                 <div className='content'>
                   <p className='title'>{port.title}</p>
-                  <h4 className='discription'>{port.discription}</h4>
+                  <h4 className='description'>{port.description}</h4>
                   <button className='btn' onClick={() => window.open(port.url)}>View</button>
                 </div>
               </div>
