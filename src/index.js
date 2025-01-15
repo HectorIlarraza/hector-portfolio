9d6d3b60e37096d5c9890983218a6f04b6a098ee
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2025-01-14 19:14:02 -0500

    added basename to router

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ColorProvider>
-      <Router>
+      <Router basename="/hector-portfolio">
         <App />
       </Router>
     </ColorProvider>
